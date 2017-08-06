6904dd92f907a2e24a5f1d95d127a07f236c48a9
1691713 <navgill1434@gmail.com>
2017-08-06 12:20:25 -0400

    ss

--- app/src/main/java/com/example/a1691713/dropapp/Main2Activity.java
+++ app/src/main/java/com/example/a1691713/dropapp/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.example.a1691713.dropapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent myNewINtent=getIntent();
+        String TheName=myNewINtent.getStringExtra("name");
+        TextView myoutput=(TextView)findViewById(R.id.output);
+
+        myoutput.setText(TheName);
     }
 }
--- app/src/main/java/com/example/a1691713/dropapp/MainActivity.java
+++ app/src/main/java/com/example/a1691713/dropapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.a1691713.dropapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +21,39 @@ public class MainActivity extends AppCompatActivity {
         String[] names={"Circle","Triangle","Square","Rectangle"};
         ArrayAdapter<String> myadapter= new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item,names);
 
+
+
         mydropdown.setAdapter(myadapter);
+    }
+
+
+    public void nextpage(View myV){
+
+
+        Spinner mydropdown=(Spinner)findViewById(R.id.dropdown);
+
+        Intent myIntent=new Intent(this,Main2Activity.class);
+        String value1=mydropdown.getSelectedItem().toString();
+        myIntent.putExtra("name",value1);
+
+        int SelectedPosition=mydropdown.getSelectedItemPosition();
+
+        switch (SelectedPosition){
+            case 0:
+                Intent myIntent1=new Intent(this,Main2Activity.class);
+
+                EditText input1=(EditText)findViewById(R.id.input1);
+                input1.setVisibility(View.INVISIBLE);
+                startActivity(myIntent);
+
+        }
+        if (SelectedPosition==0){
+            EditText input1=(EditText)findViewById(R.id.input1);
+            input1.setVisibility(View.INVISIBLE);
+            startActivity(myIntent);
+        }
+
+
+        startActivity(myIntent);
     }
 }
