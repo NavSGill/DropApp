a2f4682833cdd0c9cc2af29e95ce5ed7b47bf281
NavSGill <Navpreet1434Singh>
2017-08-10 22:28:58 -0400

    Finally Done

--- app/src/main/java/com/example/a1691713/dropapp/Main2Activity.java
+++ app/src/main/java/com/example/a1691713/dropapp/Main2Activity.java
@@ -5,6 +5,9 @@ import android.support.v4.app.NotificationCompatSideChannelService;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
@@ -18,194 +21,263 @@ public class Main2Activity extends AppCompatActivity {
         String TheName = myNewIntent.getStringExtra("name");
         TextView myoutput = (TextView) findViewById(R.id.output);
 
+        myoutput.setText("Area of"+"-"+TheName);
+
+        //checking the Selection from dropdown
+
+        EditText output1 = (EditText) findViewById(R.id.input1);
+        EditText output2 = (EditText) findViewById(R.id.input2);
+        EditText output3 = (EditText) findViewById(R.id.input3);
+        EditText output4 = (EditText) findViewById(R.id.input4);
+        EditText output5 = (EditText) findViewById(R.id.input5);
+        EditText output6 = (EditText) findViewById(R.id.input6);
+
+        switch(TheName.toString()){
+
+            case "Circle":
+
+                    output1.setVisibility(View.INVISIBLE);
+
+
+                    output2.setVisibility(View.INVISIBLE);
+
+
+                    output3.setVisibility(View.INVISIBLE);
+
+
+                    output4.setVisibility(View.VISIBLE);
+
+
+                    output5.setVisibility(View.INVISIBLE);
+
+
+                    output6.setVisibility(View.INVISIBLE);
+
+                break;
+            case "Triangle":
+
+                output1.setVisibility(View.VISIBLE);
+
+
+                output2.setVisibility(View.INVISIBLE);
+
+
+                output3.setVisibility(View.VISIBLE);
+
+
+                output4.setVisibility(View.INVISIBLE);
+
+
+                output5.setVisibility(View.INVISIBLE);
+
+
+                output6.setVisibility(View.INVISIBLE);
+                break;
+
+            case "Square":
+
+                output1.setVisibility(View.INVISIBLE);
+
+
+                output2.setVisibility(View.INVISIBLE);
+
+
+                output3.setVisibility(View.INVISIBLE);
+
+
+                output4.setVisibility(View.INVISIBLE);
+
+
+                output5.setVisibility(View.VISIBLE);
+
+
+                output6.setVisibility(View.INVISIBLE);
+                break;
+
+            case "Rectangle":
+
+                output1.setVisibility(View.INVISIBLE);
+
+
+                output2.setVisibility(View.VISIBLE);
+
+
+                output3.setVisibility(View.VISIBLE);
+
+
+                output4.setVisibility(View.INVISIBLE);
+
+
+                output5.setVisibility(View.INVISIBLE);
+
+
+                output6.setVisibility(View.INVISIBLE);
+                break;
+
+            case "Parallelogram":
+
+                output1.setVisibility(View.VISIBLE);
+
+
+                output2.setVisibility(View.INVISIBLE);
+
+
+                output3.setVisibility(View.VISIBLE);
+
+
+                output4.setVisibility(View.INVISIBLE);
+
+
+                output5.setVisibility(View.INVISIBLE);
+
+
+                output6.setVisibility(View.INVISIBLE);
+                break;
+
+            case "Trapezoid":
+
+                output1.setVisibility(View.VISIBLE);
+
+
+                output2.setVisibility(View.INVISIBLE);
+
+
+                output3.setVisibility(View.VISIBLE);
+
+
+                output4.setVisibility(View.INVISIBLE);
+
+
+                output5.setVisibility(View.VISIBLE);
+
+
+                output6.setVisibility(View.INVISIBLE);
+                break;
+
+            case "Ellipse":
+
+                output1.setVisibility(View.VISIBLE);
+
+
+                output2.setVisibility(View.INVISIBLE);
+
+
+                output3.setVisibility(View.INVISIBLE);
+
+
+                output4.setVisibility(View.INVISIBLE);
+
+
+                output5.setVisibility(View.VISIBLE);
+
+
+                output6.setVisibility(View.INVISIBLE);
+                break;
+
+            case "Sector":
+
+                output1.setVisibility(View.INVISIBLE);
+
+
+                output2.setVisibility(View.INVISIBLE);
+
+
+                output3.setVisibility(View.INVISIBLE);
+
+
+                output4.setVisibility(View.VISIBLE);
+
+
+                output5.setVisibility(View.INVISIBLE);
+
+
+                output6.setVisibility(View.VISIBLE);
+                break;
+        }
+
+        }
+//Calculate all vaideos
+
+    public void calbtn(View myv){
+
+
+        TextView addResult = (TextView)findViewById(R.id.txtResult);
+        Button btnAdd = (Button)findViewById(R.id.calA);
+        Intent myNewIntent = getIntent();
+        String TheName = myNewIntent.getStringExtra("name");
+        TextView myoutput = (TextView) findViewById(R.id.output);
+
+        EditText output1 = (EditText) findViewById(R.id.input1);
+        EditText output2 = (EditText) findViewById(R.id.input2);
+        EditText output3 = (EditText) findViewById(R.id.input3);
+        EditText output4 = (EditText) findViewById(R.id.input4);
+        EditText output5 = (EditText) findViewById(R.id.input5);
+        EditText output6 = (EditText) findViewById(R.id.input6);
+
         myoutput.setText(TheName);
 
-        //NotificationCompatSideChannelService
+        double base,width,height,radius,area,angle,result;
 
 
-        if (TheName.toString().equalsIgnoreCase("Circle")) ;
+        switch(TheName.toString())
+
         {
-            TextView output1 = (TextView) findViewById(R.id.input1);
-            output1.setVisibility(View.INVISIBLE);
+            case "Circle":
 
-            TextView output2 = (TextView) findViewById(R.id.input2);
-            output2.setVisibility(View.INVISIBLE);
-
-            TextView output3 = (TextView) findViewById(R.id.input3);
-            output3.setVisibility(View.INVISIBLE);
-
-            TextView output4 = (TextView) findViewById(R.id.input4);
-            output4.setVisibility(View.VISIBLE);
-
-            TextView output5 = (TextView) findViewById(R.id.input5);
-            output5.setVisibility(View.INVISIBLE);
-
-            TextView output6 = (TextView) findViewById(R.id.input6);
-            output6.setVisibility(View.INVISIBLE);
-
-        }
+                    radius = Double.parseDouble(output4.getText().toString());
 
 
-        if (TheName.toString().equalsIgnoreCase("Triangle")) ;
-        {
-            TextView output1 = (TextView) findViewById(R.id.input1);
-            output1.setVisibility(View.VISIBLE);
+                    result = 3.14159*radius;
+                    addResult.setText(Double.toString(result));
+                break;
+            case "Triangle":
+                base = Double.parseDouble(output1.getText().toString());
+                height = Double.parseDouble(output3.getText().toString());
 
-            TextView output2 = (TextView) findViewById(R.id.input2);
-            output2.setVisibility(View.INVISIBLE);
+                result=0.5*base*height;
+                addResult.setText(Double.toString(result));
+                break;
 
-            TextView output3 = (TextView) findViewById(R.id.input3);
-            output3.setVisibility(View.VISIBLE);
+            case"Square":
+                area=Double.parseDouble(output5.getText().toString());
+                result=area*area;
+                addResult.setText(Double.toString(result));
+                break;
+            case "Rectangle":
+                width=Double.parseDouble(output2.getText().toString());
+                height=Double.parseDouble(output3.getText().toString());
 
-            TextView output4 = (TextView) findViewById(R.id.input4);
-            output4.setVisibility(View.INVISIBLE);
+                result=width*height;
+                addResult.setText(Double.toString(result));
+                break;
+            case "Parallelogram":
+                base=Double.parseDouble(output1.getText().toString());
+                height=Double.parseDouble(output3.getText().toString());
 
-            TextView output5 = (TextView) findViewById(R.id.input5);
-            output5.setVisibility(View.INVISIBLE);
+                result=base*height;
+                addResult.setText(Double.toString(result));
+                break;
+            case "Trapezoid":
+                area=Double.parseDouble(output5.getText().toString());
+                base=Double.parseDouble(output1.getText().toString());
+                height=Double.parseDouble(output3.getText().toString());
 
-            TextView output6 = (TextView) findViewById(R.id.input6);
-            output6.setVisibility(View.INVISIBLE);
+                result=0.5*(area+base)*height;
+                addResult.setText(Double.toString(result));
+                break;
+            case "Ellipse":
+                area=Double.parseDouble(output5.getText().toString());
+                base=Double.parseDouble(output1.getText().toString());
+                result=3.14159*area*base;
+                addResult.setText(Double.toString(result));
+                break;
+            case "Sector":
+                radius = Double.parseDouble(output4.getText().toString());
+                angle= Double.parseDouble(output6.getText().toString());
+                result=0.5*radius*radius*angle;
+                addResult.setText(Double.toString(result));
+                break;
+     }
 
-             }
-//
-            // if(TheName.toString().equals("Square"));
-            // {
-            //     TextView output1=(TextView)findViewById(R.id.input1);
-            //     output1.setVisibility(View.INVISIBLE);
-//
-            //     TextView output2=(TextView)findViewById(R.id.input2);
-            //     output2.setVisibility(View.INVISIBLE);
-//
-            //     TextView output3=(TextView)findViewById(R.id.input3);
-            //     output3.setVisibility(View.INVISIBLE);
-//
-            //     TextView output4=(TextView)findViewById(R.id.input4);
-            //     output4.setVisibility(View.INVISIBLE);
-//
-            //     TextView output5=(TextView)findViewById(R.id.input5);
-            //     output5.setVisibility(View.VISIBLE);
-//
-            //     TextView output6=(TextView)findViewById(R.id.input6);
-            //     output6.setVisibility(View.INVISIBLE);
-//
-//
-            // }
-//
-            // if(TheName.toString().equals("Rectangle"));
-            // {
-            //     TextView output1=(TextView)findViewById(R.id.input1);
-            //     output1.setVisibility(View.INVISIBLE);
-//
-            //     TextView output2=(TextView)findViewById(R.id.input2);
-            //     output2.setVisibility(View.VISIBLE);
-//
-            //     TextView output3=(TextView)findViewById(R.id.input3);
-            //     output3.setVisibility(View.VISIBLE);
-//
-            //     TextView output4=(TextView)findViewById(R.id.input4);
-            //     output4.setVisibility(View.INVISIBLE);
-//
-            //     TextView output5=(TextView)findViewById(R.id.input5);
-            //     output5.setVisibility(View.INVISIBLE);
-//
-            //     TextView output6=(TextView)findViewById(R.id.input6);
-            //     output6.setVisibility(View.INVISIBLE);
-//
-//
-//
-            // }
-            // if(TheName.toString().equals("Parallelogram"));
-            // {
-            //     TextView output1=(TextView)findViewById(R.id.input1);
-            //     output1.setVisibility(View.VISIBLE);
-//
-            //     TextView output2=(TextView)findViewById(R.id.input2);
-            //     output2.setVisibility(View.INVISIBLE);
-//
-            //     TextView output3=(TextView)findViewById(R.id.input3);
-            //     output3.setVisibility(View.VISIBLE);
-//
-            //     TextView output4=(TextView)findViewById(R.id.input4);
-            //     output4.setVisibility(View.INVISIBLE);
-//
-            //     TextView output5=(TextView)findViewById(R.id.input5);
-            //     output5.setVisibility(View.INVISIBLE);
-//
-            //     TextView output6=(TextView)findViewById(R.id.input6);
-            //     output6.setVisibility(View.INVISIBLE);
-//
-            // }
-//
-            // if(TheName.toString().equals("Trapezoid"));
-            // {
-            //     TextView output1=(TextView)findViewById(R.id.input1);
-            //     output1.setVisibility(View.VISIBLE);
-//
-            //     TextView output2=(TextView)findViewById(R.id.input2);
-            //     output2.setVisibility(View.INVISIBLE);
-//
-            //     TextView output3=(TextView)findViewById(R.id.input3);
-            //     output3.setVisibility(View.VISIBLE);
-//
-            //     TextView output4=(TextView)findViewById(R.id.input4);
-            //     output4.setVisibility(View.INVISIBLE);
-//
-            //     TextView output5=(TextView)findViewById(R.id.input5);
-            //     output5.setVisibility(View.VISIBLE);
-//
-            //     TextView output6=(TextView)findViewById(R.id.input6);
-            //     output6.setVisibility(View.INVISIBLE);
-//
-//
-            // }
-            // if(TheName.toString().equals("Ellipse"));
-            // {
-            //     TextView output1=(TextView)findViewById(R.id.input1);
-            //     output1.setVisibility(View.VISIBLE);
-//
-            //     TextView output2=(TextView)findViewById(R.id.input2);
-            //     output2.setVisibility(View.INVISIBLE);
-//
-            //     TextView output3=(TextView)findViewById(R.id.input3);
-            //     output3.setVisibility(View.INVISIBLE);
-//
-            //     TextView output4=(TextView)findViewById(R.id.input4);
-            //     output4.setVisibility(View.INVISIBLE);
-//
-            //     TextView output5=(TextView)findViewById(R.id.input5);
-            //     output5.setVisibility(View.VISIBLE);
-//
-            //     TextView output6=(TextView)findViewById(R.id.input6);
-            //     output6.setVisibility(View.INVISIBLE);
-//
-//
-            // }
-//
-            // if(TheName.toString().equals("Sector"));
-            // {
-            //     TextView output1=(TextView)findViewById(R.id.input1);
-            //     output1.setVisibility(View.INVISIBLE);
-//
-            //     TextView output2=(TextView)findViewById(R.id.input2);
-            //     output2.setVisibility(View.INVISIBLE);
-//
-            //     TextView output3=(TextView)findViewById(R.id.input3);
-            //     output3.setVisibility(View.INVISIBLE);
-//
-            //     TextView output4=(TextView)findViewById(R.id.input4);
-            //     output4.setVisibility(View.VISIBLE);
-//
-            //     TextView output5=(TextView)findViewById(R.id.input5);
-            //     output5.setVisibility(View.INVISIBLE);
-//
-            //     TextView output6=(TextView)findViewById(R.id.input6);
-            //     output6.setVisibility(View.VISIBLE);
-//
-//
-//
-            // }
-
-
-        }
-
+    }
     }
